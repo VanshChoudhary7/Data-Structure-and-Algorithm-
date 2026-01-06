5c749a613e1da0d14d260897c33163b5cd74687b
Vansh Choudhary <Iamvanshchaudhary7@gmail.com>
2026-01-06 10:18:23 +0530

    Selection Sort

--- Selection_sort.java
+++ Selection_sort.java
@@ -4,14 +4,16 @@ public class Selection_sort {
         int len = Data.len;
         int i,j;
 
-        for(i = 0; i < len-1 ; i++) {
-            for(j = i+1; j < len; j++){
-                if(arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        for(i = 0; i < len-1; i++) {
+            int min = i;
+            for(j = i+1; j < len; j++) {
+                if(arr[j] < arr[min]) {
+                    min = j;
                 }
             }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
-    }
+} 
 }
