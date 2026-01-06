301a18f811991ec40b89fdcdc90308f395fea8d9
Vansh Choudhary <Iamvanshchaudhary7@gmail.com>
2026-01-06 10:19:05 +0530

    Bubble Sort Done

---
+++ Bubble_sort.java
@@ -0,0 +1,19 @@
+public class Bubble_sort {
+    public static void sort() {
+
+        int[] arr = Data.arr;
+        int len = Data.len;
+
+        int i,j;
+
+        for(i = 0; i < len; i++) {
+            for(j = 0; j < len - i-1; j++) {
+                if(arr[j] > arr[j+1] ) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String args[]) {
-        Selection_sort.sort();
+        
+        // Selection_sort.sort();
+        Bubble_sort.sort();
         PrintArr.print();
     }
 }
