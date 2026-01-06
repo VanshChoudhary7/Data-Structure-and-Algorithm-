365f02b3d30c683fa4a894f8cc9026e2cba1dae1
Vansh Choudhary <Iamvanshchaudhary7@gmail.com>
2026-01-06 11:20:30 +0530

    Optimized Bubble Sort

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -7,13 +7,16 @@ public class Bubble_sort {
         int i,j;
 
         for(i = 0; i < len; i++) {
+            boolean swapped = false;
             for(j = 0; j < len - i-1; j++) {
                 if(arr[j] > arr[j+1] ) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
             }
+            if(!swapped) break;
         }
     }
 }
